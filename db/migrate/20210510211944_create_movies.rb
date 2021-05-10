3d05e4db93162f9223730f3fd09b08df08679e90
Your Username <you@example.com>
2021-05-10 17:21:44 -0400

    ran rails g resource for Actor and Movie

---
+++ db/migrate/20210510211944_create_movies.rb
@@ -0,0 +1,17 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.integer :year
+      t.string :rated
+      t.string :realeased
+      t.string :genre
+      t.string :director
+      t.string :writer
+      t.string :language
+      t.string :box-office
+
+      t.timestamps
+    end
+  end
+end
