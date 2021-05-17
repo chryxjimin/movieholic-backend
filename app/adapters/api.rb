afc50bc8de823cb7858cd25746cb626bc2abe7ec
Your Username <you@example.com>
2021-05-17 10:53:41 -0400

    updated Api class to render data

--- app/adapters/api.rb
+++ app/adapters/api.rb
@@ -11,15 +11,20 @@ class Api
     
     def get_movies
         # puts API_KEY
-        url = "http://www.omdbapi.com/?t=life+in+a+metro&apikey=#{API_KEY}"
-        binding.pry
+        url = "http://www.omdbapi.com/?s=Batman&apikey=#{API_KEY}"
         uri = URI.parse(url)
         response_body = uri.read
         data = JSON.parse(response_body)
-        
+        data["Search"].each do |movie|
+            title = movie["Title"]
+            year = movie["Year"]
+            poster = movie["Poster"]
+            Movie.create(title: movie["Title"], year: movie["Year"], poster: movie["Poster"])
+            # binding.pry
+        end
     end
 
 end
 
 
-Api.new.get_movies
+# Api.new.get_movies
